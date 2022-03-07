9cce6ffa127f10552389ba82ac87287f8ec2cdd6
ltx <lyx09130526@163.com>
2022-03-07 19:09:13 +0800

    update

--- cloud_info_flask/common/models/users.py
+++
@@ -1,8 +0,0 @@
-from common.models import db
-
-
-class User(db.Model):
-    __tablename__ = 'name'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(32), doc='用户名')
-    age = db.Column(db.Integer, doc='年龄')
--- cloud_info_flask/project/main.py
+++
@@ -1,7 +0,0 @@
-from project.resources import create_flask_app
-from common.settings.sql_config import sqlconfig
-
-app = create_flask_app(sqlconfig)
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', 5000)
